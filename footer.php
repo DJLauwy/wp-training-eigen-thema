b59af03f4e76e0ea88526d8311c430adf940bd7f
DJLauwy <laudehoop@hotmail.com>
2020-07-10 14:22:50 +0200

    update
    filmpje 13 afgerond

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
-		<footer>
-			<small>
-				&copy; <?php the_author();
-				echo ' ' . date('Y'); ?>
-			</small>
+		<footer class="bg-primary text-white p-4">
+			<div class="container">
+				<small>
+					&copy; <?php the_author();
+					echo ' ' . date('Y'); ?>
+				</small>
+			</div>
 		</footer>
 		<?php wp_footer(); ?>
 	</body>
